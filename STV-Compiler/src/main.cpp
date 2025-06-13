c41b5274f04d4bf02fc8312532bbbbd4bb9f93b3
absumit <sugarfree7970@gmail.com>
2025-06-14 03:31:40 +0530

    feat: print all tokens including Print from test.stv in main.cpp

--- STV-Compiler/src/main.cpp
+++ STV-Compiler/src/main.cpp
@@ -1,6 +1,37 @@
-// Entry point for the compiler
+#include "../include/lexer.h"
+#include <iostream>
+
+std::string tokenTypeToString(TokenType type)
+{
+    switch (type)
+    {
+    case TokenType::Identifier:
+        return "Identifier";
+    case TokenType::Number:
+        return "Number";
+    case TokenType::String:
+        return "String";
+    case TokenType::Equals:
+        return "Equals";
+    case TokenType::Plus:
+        return "Plus";
+    case TokenType::Semicolon:
+        return "Semicolon";
+    case TokenType::Print:
+        return "Print";
+    case TokenType::EndOfFile:
+        return "EndOfFile";
+    default:
+        return "Unknown";
+    }
+}
+
 int main()
 {
-    // ...existing code...
+    auto tokens = tokenize("STV-Compiler/tests/test.stv");
+    for (const auto &token : tokens)
+    {
+        std::cout << tokenTypeToString(token.type) << ": '" << token.value << "'\n";
+    }
     return 0;
 }
