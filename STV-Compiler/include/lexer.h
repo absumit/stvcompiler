3887a28d1efca76c2804edd7a9e2d7eaa44b478a
absumit <sugarfree7970@gmail.com>
2025-06-14 03:31:02 +0530

    feat: define TokenType enum and Token struct, add Print token support in lexer.h

--- STV-Compiler/include/lexer.h
+++ STV-Compiler/include/lexer.h
@@ -1,2 +1,26 @@
-// Lexer header
-// ...existing code...
+#ifndef LEXER_H
+#define LEXER_H
+#include <string>
+#include <vector>
+
+enum class TokenType
+{
+    Identifier,
+    Number,
+    String,
+    Equals,
+    Plus,
+    Semicolon,
+    Print,
+    EndOfFile
+};
+
+struct Token
+{
+    TokenType type;
+    std::string value;
+};
+
+std::vector<Token> tokenize(const std::string &filename);
+
+#endif // LEXER_H
